dd69c54107634cf3ac71ee9edccbbb480db64fb0
Greg Silcox <greg.silcox@gmail.com>
2023-11-16 19:13:07 -0800

    Updated some policies and views to hide buttons

--- app/policies/microgreens/green_policy.rb
+++ app/policies/microgreens/green_policy.rb
@@ -7,5 +7,25 @@ module Microgreens
     def show?
       true
     end
+
+    def new?
+      user&.admin?
+    end
+
+    def create?
+      user&.admin?
+    end
+
+    def edit?
+      user&.admin?
+    end
+
+    def update?
+      user&.admin?
+    end
+
+    def destroy?
+      user&.admin?
+    end
   end
 end
--- app/policies/microgreens/product_policy.rb
+++ app/policies/microgreens/product_policy.rb
@@ -3,5 +3,29 @@ module Microgreens
     def index?
       true
     end
+
+    def show?
+      true
+    end
+
+    def new?
+      user&.admin?
+    end
+
+    def create?
+      user&.admin?
+    end
+
+    def edit?
+      user&.admin?
+    end
+
+    def update?
+      user&.admin?
+    end
+
+    def destroy?
+      user&.admin?
+    end
   end
 end
